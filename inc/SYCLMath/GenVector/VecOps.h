4a8fc25acea22cee197a73e223713f4690436998
mdessole <monicadessole@gmail.com>
2025-03-14 16:05:02 +0100

    Remove extra CUDA function

--- inc/SYCLMath/GenVector/VecOps.h
+++ inc/SYCLMath/GenVector/VecOps.h
@@ -196,53 +196,6 @@ namespace ROOT {
       return invMasses;
     }
 
-
-    template <class Scalar, class LVector>
-    Scalar *InvariantMasses(LVector *v1, LVector *v2, const size_t N,
-                            const size_t local_size)
-    {
-
-      Scalar *invMasses = new Scalar[N];
-
-      // Allocate device input vector
-      LVector *d_v1 = NULL;
-      ERRCHECK(cudaMalloc((void **)&d_v1, N * sizeof(LVector)));
-
-      // Allocate device input vector
-      LVector *d_v2 = NULL;
-      ERRCHECK(cudaMalloc((void **)&d_v2, N * sizeof(LVector)));
-
-      // Allocate the device output vector
-      Scalar *d_invMasses = NULL;
-      ERRCHECK(cudaMalloc((void **)&d_invMasses, N * sizeof(Scalar)));
-
-#ifdef ROOT_MEAS_TIMING
-      auto start = std::chrono::system_clock::now();
-#endif
-
-      cudaMemcpy(d_v1, v1, N * sizeof(LVector), cudaMemcpyHostToDevice);
-      cudaMemcpy(d_v2, v2, N * sizeof(LVector), cudaMemcpyHostToDevice);
-
-      InvariantMassesKernel<<<N / local_size + 1, local_size>>>(d_v1, d_v2, d_invMasses, N);
-
-      ERRCHECK(cudaMemcpy(invMasses, d_invMasses, N * sizeof(Scalar), cudaMemcpyDeviceToHost));
-
-#ifdef ROOT_MEAS_TIMING
-      auto end = std::chrono::system_clock::now();
-      auto duration =
-          std::chrono::duration_cast<std::chrono::microseconds>(end - start)
-              .count() *
-          1e-6;
-      std::cout << "cuda time " << duration << " (s)" << std::endl;
-#endif
-      ERRCHECK(cudaFree(d_v1));
-      ERRCHECK(cudaFree(d_v2));
-      ERRCHECK(cudaFree(d_invMasses));
-
-      return invMasses;
-    }
-
-
     template <class Scalar, class LVector>
     Scalar *InvariantMasses(LVector *v1, LVector *v2, const size_t N,
                             const size_t local_size, cudaStream_t queue)
